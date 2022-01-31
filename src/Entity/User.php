28ee01e75a03d6072a791eecfece24269762ae66
Loris <45939615+lchastanet@users.noreply.github.com>
2022-01-31 16:13:51 +0100

    Bootstrap symfony, first entity and vue

---
+++ src/Entity/User.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $login;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Pass;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLogin(): ?string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): self
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    public function getPass(): ?string
+    {
+        return $this->Pass;
+    }
+
+    public function setPass(string $Pass): self
+    {
+        $this->Pass = $Pass;
+
+        return $this;
+    }
+}
